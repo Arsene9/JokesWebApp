f76ede485144d33b68bbbab63a85c9b224bc04d1
Arsene Foka <arsene.foka@yahoo.fr>
2023-08-31 22:02:35 -0500

    New Adjustments

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,11 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public required string JokeQuestion { get; set; }
+        public required string JokeAnswer { get; set; }
+
+        public Joke() { }
+    }
+}
